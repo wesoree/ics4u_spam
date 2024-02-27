49e1257bec69128970104ffc98147fca0b28b6b8
wesoree <wesleylo174@gmail.com>
2024-02-27 17:29:38 -0500

    update testing.cs

--- testing.cs
+++ testing.cs
@@ -1,3 +1,8 @@
-Random rnd = new Random();
-int dice = rnd.Next(1, 7);
-
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int dice = rnd.Next(1, 7);
+    }
+}
